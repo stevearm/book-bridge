a8ef3941e1210060366aa3c7e1fc10ada076b301
Steve Armstrong <steve@horsefire.com>
2017-01-08 14:44:29 -0800

    Figure out modules

--- bin/list-shelves.js
+++ bin/list-shelves.js
@@ -1,34 +1,4 @@
-const fs = require('fs');
-const rp = require("request-promise")
-
-function readKeys() {
-	return new Promise(function (resolve, reject) {
-        fs.readFile(__dirname + "/../data/keys.json", function (error, data) {
-            if (error) {
-                console.log(error);
-                return reject(error);
-            }
-
-            var keys = JSON.parse(data);
-            resolve(keys);
-        });
-    });
-}
-
-function listShelves(userId) {
-    return readKeys().then((keys) => {
-        return rp({
-            method: "GET",
-            uri: "https://www.goodreads.com/shelf/list.xml",
-            qs: {
-                key: keys.key,
-                user_id: userId
-            }
-        });
-    }).then((list) => {
-        return list;
-    }).catch((err) => console.log("Error", err));
-}
+const goodreads = require("../lib/goodreads")
 
 if (process.argv.length < 3) {
     console.log("Must supply a user-id");
@@ -37,6 +7,6 @@ if (process.argv.length < 3) {
 
 var userId = process.argv[2];
 
-listShelves(userId).then((list) => {
+goodreads.listShelves(userId).then((list) => {
     console.log("Final output is", list);
 });
